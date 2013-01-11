41cd5e00d423e4131a807f65d237c4feafd2ace3
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-01-11 23:20:59 +0900

    writing dummyTimer, instead of Timer.

--- src/timer.js
+++ src/timer.js
@@ -1,5 +1,5 @@
 /**
- * Rajah.Timer
+ * Rajah.dummyTimer
  *
  */
 var RajahQue = [];
@@ -25,19 +25,97 @@ var setTimeout = function (f, t) {
 
   RajahQue.push({func: f});
   return RajahQue.length;
-//  Utilities.sleep(t);
-//  try {
-//    f();
-//  } catch (e) {
-//    ;
-//  }
 };
 
-var clearTimeout = function () {};
+var clearTimeout = function (id) {};
 
-var setInterval = function () {};
+var setInterval = function (f, t) {};
 
-var clearInterval = function () {};
+var clearInterval = function (id) {};
 
 
+Rajah.dummyTimer = (function () {
 
+  var timerQue =[];
+  var idCounter = 0;
+
+  var clear = function () {
+    timerQue.length = 0;
+    // idCounter is NOT initialised here.
+    // That's for keeping id unique, although after timer cleared.
+  };
+
+  var execute = function () {
+    
+    var now = new Date();
+
+    // while task exists,
+    while (timerQue.length > 0) {
+
+      if (timerQue[0].time <= now) {
+        timerQue[0].func();
+        if (timerQue[0].t > 0) {
+          // Re-entry (id, func, t)
+          ;
+        }
+        timerQue.shift();   // delete entry that has been called.
+
+      } else {
+        // wait a while;
+        sleep(timerQue[0].time - now);
+      }
+    }
+  };
+  
+  var setTimeout = function (func, t) {
+  
+    var id = idCounter++;
+  
+    // not push. into order
+    timerQue.push({
+      id:       id,
+      func:     func,
+      time:     t + now,    // now should replace with like 'new Date'
+      interval: 0
+    });
+
+    return id;
+  };
+  
+  var clearTimeout = function () {};
+  
+
+  var setInterval = function (func, t) {
+
+    if (t <= 0) {
+      return -1;    // Error: t should be ' > 0 '
+    }
+
+    var id = idCounter++;
+  
+    // not push. into order
+    timerQue.push({
+      id:       id,
+      func:     func,
+      time:     t + now,    // now should replace with like 'new Date'
+      interval: t
+    });
+
+    return id;
+  };
+  
+  var clearInterval = function () {};
+
+  
+  var exports = {
+    clear:          clear,
+    execute:        execute,
+
+    setTimeout:     setTimeout,
+    clearTimeout:   clearTimeout,
+    setInterval:    setInterval,
+    clearInterval:  clearInterval,
+  };
+  return exports;
+
+})();
