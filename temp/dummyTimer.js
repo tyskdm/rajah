646a392a1cc975da9a95f0f9c9f7ca164a9420ba
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-25 13:28:05 +0900

    move/remove files.

--- temp/dummyTimer.js
+++ temp/dummyTimer.js
@@ -1,3 +1,4 @@
+
 /**
  * rajah.dummyTimer
  *
@@ -33,10 +34,10 @@ rajah.dummyTimer = (function () {
 
   var idCounter = 0;
   var timerQue =[];
-  
+
   var timerQue_ins_ = function (o) {
     var i;
-    
+
     for (i = timerQue.length-1; i >= 0; i--) {
       if (timerQue[i].time > o.time) {
         continue;
@@ -47,10 +48,10 @@ rajah.dummyTimer = (function () {
     // o should be inserted to next of 'i'.
     timerQue.splice(i+1, 0, o);
   };
-  
+
   var timerQue_del_ = function (id) {
     var i;
-    
+
     for (i = timerQue.length-1; i >= 0; i--) {
       if (timerQue[i].id == id) {
         timerQue.splice(i, 1);
@@ -58,7 +59,7 @@ rajah.dummyTimer = (function () {
       }
     }
   };
-  
+
   var clear = function () {
     timerQue.length = 0;
     // idCounter is NOT initialised here.
@@ -66,11 +67,11 @@ rajah.dummyTimer = (function () {
   };
 
   var execute = function () {
-    
+
     // while task exists,
     while (timerQue.length > 0) {
       var now = new Date().getTime();
-      
+
       if (now >= timerQue[0].time) {
         var o = timerQue.shift();
         o.func();
@@ -84,11 +85,11 @@ rajah.dummyTimer = (function () {
       }
     }
   };
-  
+
   var setTimeout = function (func, t) {
-  
+
     var id = idCounter++;
-  
+
     timerQue_ins_({
       id:       id,
       func:     func,
@@ -98,11 +99,11 @@ rajah.dummyTimer = (function () {
 
     return id;
   };
-  
+
   var clearTimeout = function (id) {
     timerQue_del_(id);
   };
-  
+
 
   var setInterval = function (func, t) {
 
@@ -111,7 +112,7 @@ rajah.dummyTimer = (function () {
     }
 
     var id = idCounter++;
-  
+
     timerQue_ins_({
       id:       id,
       func:     func,
@@ -121,12 +122,12 @@ rajah.dummyTimer = (function () {
 
     return id;
   };
-  
+
   var clearInterval = function () {
     timerQue_del_(id);
   };
 
-  
+
   var exports = {
     clear:          clear,
     execute:        execute,
