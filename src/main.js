6a03f71b6f7bdc3a641743911f9723d08f4cda1c
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-04-04 19:05:23 +0900

    add javadoc notation for gas-lib.

--- src/main.js
+++ src/main.js
@@ -8,21 +8,37 @@ this.timers = require('./timers');
 this.rajah = require('./rajah.js');
 var jasmine = this.rajah.jasmine;
 
+/**
+ * Initialize rajah and get Jasmine-API.
+ * @param {object} THIS global scope object.
+ */
 function init(THIS) {
     this.GLOBAL = THIS;
     return this.rajah.setup(THIS);
 }
 
+/**
+ * Wrapper function for jasmine.addReporter.
+ * @param {object} reporter
+ */
 function addReporter(reporter) {
     return this.rajah.addReporter(reporter);
 }
 
+/**
+ * Setup original Console-reporter with original print function.
+ * @param {boolean} showColors
+ * @param {function} print function print(str) should puts string without trailing '\n'.
+ */
 function addConsoleReporter(showColors, print) {
     return this.rajah.addConsoleReporter(showColors, print);
 }
 
+/**
+ * Execute Jasmine.
+ * @param {function} onComplete function onComplete(passed).
+ */
 function run(onComplete) {
     this.rajah.run(onComplete);
     this.timers.execute();
 }
-
